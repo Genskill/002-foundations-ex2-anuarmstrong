a26080d720c52b56ef02b19ca126b50880bd5c4e
Anu Armstrong <84122016+anuarmstrong@users.noreply.github.com>
2021-05-18 21:19:33 +0530

    Create patterns.c

---
+++ patterns.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <cs50.h>
+int main(void) 
+{
+  int i,j,k;
+  int option = get_int("Enter type of pattern (1 or 2) ");
+  int rows = get_int("Number of rows ");
+  switch(option) 
+  {
+    case 1:
+    	for(i=0;i<rows;i++)
+    	{
+    	  for(j=rows-i;j>0;j--)
+    	  {
+    	    printf("#");
+    	  }
+	      printf("\n");
+	    }
+      break;
+    case 2:
+    	for(i=0;i<rows;i++)
+    	{
+    	  for(j=rows-(i+1),k=0;j>0;k++,j--)
+    	  {
+    	    printf(" ");
+	      }
+	      for(j=k;j<rows;j++)
+	      {
+	        printf("#");
+	      }
+          printf("\n");
+	    }
+      break;
+  }
+return 0; 
+}
